83bf4b8136e8906972cc88f5976138ac8f2af440
mayurshelar-coditas <158130993+mayurshelar-coditas@users.noreply.github.com>
2024-02-02 10:54:56 +0530

    Update day5Assignment.js

--- JavaScript Assignments/day5Assignment.js
+++ JavaScript Assignments/day5Assignment.js
@@ -7,5 +7,11 @@ function feast(beast, dish) {
       } 
       
       return false;
+
+    //Optimized Approach
+    // if(beast[0] === dish[0] && beast[beast.length-1] === dish[dish.length-1]){
+    //     return true;
+    // } 
+    // return false;
     }
 
