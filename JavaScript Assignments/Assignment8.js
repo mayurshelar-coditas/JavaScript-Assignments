c6e5bbcfa485cfe7bf77bd42762a97a43d78bdf4
mayurshelar-coditas <158130993+mayurshelar-coditas@users.noreply.github.com>
2024-02-07 13:54:14 +0530

    Update Assignment8.js

--- JavaScript Assignments/Assignment8.js
+++ JavaScript Assignments/Assignment8.js
@@ -1,17 +1,19 @@
 function getLengthOfMissingArray(arrayOfArrays) {
-  if(!Array.isArray(arrayOfArrays) || arrayOfArrays === null || arrayOfArrays.length === 0){
+  if(arrayOfArrays === null || arrayOfArrays.length === 0){
     return 0;
   }
   if (arrayOfArrays.some(arr => arr === null || arr === undefined)) {
     return 0;
   }
   arrayOfArrays.sort(myFunction = (a,b) => a.length - b.length);
+  
   for(let index=0; index < arrayOfArrays.length-1; index++){
-    let nestedArray1 = arrayOfArrays[index];
-    let nestedArray2 = arrayOfArrays[index+1];
+    let nestedArray1 = arrayOfArrays[index], nestedArray2 = arrayOfArrays[index+1];
+    
     if(nestedArray1.length === 0 || nestedArray2.length === 0){
       return 0;
     }
+    
     if(parseInt(nestedArray1.length) !== parseInt(nestedArray2.length-1)){
       return nestedArray1.length+1;
     }
