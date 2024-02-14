7e89f7bafb9d482b4c9475a9c0784c827e05b0b8
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-02-14 12:52:19 +0530

    day 13 classwork

---
+++ JavaScript Assignments/day13ClassWork.js
@@ -0,0 +1,19 @@
+//High Order function.
+
+const addition = (firstOperand, secondOperand) => {
+    return firstOperand + secondOperand;
+} 
+
+const map = ((array, callback) => {
+        const modifiedArray = [];
+        for(const number in numbersArray){
+            modifiedArray[number] = numbersArray[number]**2;
+        }
+        return modifiedArray;
+});
+
+
+
+const squareArray = (displaySum([1,2,3,4,5]));
+console.log(squareArray);
+
