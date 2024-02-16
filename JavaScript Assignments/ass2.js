1c93023fd3587074fd5d5fb1a2c3d3f11a4460fa
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-02-16 11:29:20 +0530

    fsd

---
+++ JavaScript Assignments/ass2.js
@@ -0,0 +1,26 @@
+const loanCalculator = (rateOfInterest) => { 
+    return [(updatedRate) => (10000*updatedRate*12)/100, () => (1000000*rateOfInterest*12)/100]
+} 
+
+const homeLoan = loanCalculator(10);
+const [updatRateForHomeLoan, displayhomeLoan] = homeLoan;
+
+console.log(displayhomeLoan());
+console.log(updatRateForHomeLoan(12));
+
+const carLoan = loanCalculator(10);
+const [updatRateForCarLoan, displayCarLoan] = carLoan;
+
+console.log(displayCarLoan());
+console.log(updatRateForCarLoan(19));
+
+const educationLoan = loanCalculator(10);
+const [updatRateForEducationLoan, displayEducationLoan] = educationLoan;
+
+console.log(displayEducationLoan());
+console.log(updatRateForEducationLoan(14));
+
+
+
+
+
