45051aa393586dbbd8a1a515849a65049ee412e1
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-02-22 14:47:31 +0530

    Assignment

--- JavaScript Assignments/day20Assignment.js
+++ JavaScript Assignments/day20Assignment.js
@@ -1,39 +1,41 @@
 // Assignment - 17 (Promises)
 // DEADLINE - 21st Feb 2025, 9:29AM
-const countriesAPI = 'https://restcountries.com/v2/all'
-const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+const countriesAPI = 'https://restcountries.com/v2/all';
+
 // Read the countries API using fetch and print the name of country, capital, languages, population and area.
-const countryDetails = new Promise((resolve, reject) => {
-    return fetch(countriesAPI);
-});
 
-countryDetails.then(response => response).then(data => data.forEach((country) => {
-    const {name, capital, languages, poplulation} = country;
-    console.log(`Name: ${name}, Capital: ${capital}, languages: ${languages}, Population: ${poplulation}`);
-})).catch("Error fetching data");
+const countryDetails = async () => {
+        const response = await fetch(countriesAPI);
+        const countries = await response.json();
+        countries.forEach(country => {
+            const { name, capital, languages, population, area } = country;
+            console.log(`Name: ${name}`);
+            console.log(`Capital: ${capital}`);
+            console.log(`Languages: ${languages.map(language => language.name).join(', ')}`); 
+            console.log(`Population: ${population}`);
+            console.log(`Area: ${area}`);
+        });
+        return countries;
+};
 
-// Print out all the cat names in to catNames variable.
-const cats = new Promise((resolve, reject) => {
-    fetch(catsAPI)
-        .then(response => response.json())
-        .then(data => resolve(data))
-        .catch(error => reject(error));
-});
+const countries = countryDetails();
 
-cats.then(data => {
-    data.forEach(cat => console.log(`Cat name: ${cat.name}`));
-}).catch(error => {
-    console.log("Something went wrong", error);
-});
-
-// Read the cats api and find the average weight of cat in metric unit.
-
-// cats.then(data => {
-//     data.forEach(cat => console.log(`Cat name: ${cat.name}`));
-// }).catch(error => {
-//     console.log("Something went wrong", error);
-// });
 // Read the countries api and find out the 10 largest countries
-// Read the countries api and count total number of languages in the world used as officials.
+console.log();
+
+const topTenLargestCountries = async () => {
+      const countriesData = await countries;
+        const topTenCountries = countriesData
+        .sort((a, b) => b.area - a.area)
+        .slice(0, 10);
+  
+      console.log('The 10 largest countries by area are:');
+      topTenCountries.forEach(country => {
+        const {name, area} = country;
+        console.log(`${name} has an Area of ${area} sq. km`);
+      });
+  };
+  
+topTenLargestCountries();
 
 
